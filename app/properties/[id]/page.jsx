19cc49be4c40e1a97c3f3ed951f6b07b9276fba6
mv0x19 <49604532+mv0x19@users.noreply.github.com>
2024-03-29 06:49:04 +0200

    clean property page for future development

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,38 +1,9 @@
 'use client';
 
-import {
-  useRouter,
-  useParams,
-  useSearchParams,
-  usePathname,
-} from 'next/navigation';
-
 const PropertyPage = () => {
-  console.log('client');
-  const router = useRouter();
-  const { id } = useParams();
-  const searchParams = useSearchParams();
-  const name = searchParams.get('name');
-  const pathname = usePathname();
-
   return (
     <div>
       <p>PropertyPage</p>
-      {/* http://localhost:3000/properties/1 */}
-      <p>id form useParams: {id}</p>
-      {/* http://localhost:3000/properties/1?name=John */}
-      <p>
-        {!name
-          ? 'name from searchParams: (press Test button)'
-          : `name from searchParams: ${name}`}
-      </p>
-      <p>pathname: {pathname}</p>
-      <button
-        onClick={() => router.push('/properties/1?name=John')}
-        type='button'
-        className=' ml-1 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
-        Test
-      </button>
     </div>
   );
 };
