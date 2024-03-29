b39511e2f5f5ce708f7f26702378beaa333e8bb4
mv0x19 <49604532+mv0x19@users.noreply.github.com>
2024-03-29 08:47:11 +0200

    create properties page and property card component

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,10 +1,22 @@
-import Link from 'next/link';
+import PropertyCard from '@/components/PropertyCard';
+import properties from '@/properties.json';
 
 const PropertiesPage = () => {
   return (
     <div>
-      <p>PropertiesPage</p>
-      <Link href='/'>&lt; Go Home</Link>
+      <section className='px-4 py-6'>
+        <div className='container-xl m-auto px-4 py-6 lg:container'>
+          {properties.length === 0 ? (
+            <p>No properties found</p>
+          ) : (
+            <div className='grid grid-cols-1 gap-6 md:grid-cols-3'>
+              {properties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
     </div>
   );
 };
