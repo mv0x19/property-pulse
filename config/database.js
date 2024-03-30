2ed1f04ab7984f412213d9fac2474738ffd4b942
mv0x19 <49604532+mv0x19@users.noreply.github.com>
2024-03-30 15:46:08 +0200

    add mongodb

---
+++ config/database.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+let connected = false;
+
+const connectDB = async () => {
+  // only fields specified in scheme will be saved in database if strictQuery is true
+  mongoose.set('strictQuery', true);
+  // if connected to MongoDB, don't connect again
+  if (connected) {
+    console.log('MongoDB is already connected.');
+    return;
+  }
+  // connect to MongoDB
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    connected = true;
+    console.log('MongoDB is connected.');
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+export default connectDB;
