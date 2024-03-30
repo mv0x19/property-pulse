e4f83c5e07bbe00f4f1bc54bd726009101e298d2
mv0x19 <49604532+mv0x19@users.noreply.github.com>
2024-03-31 00:11:34 +0200

    fix typo

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 let connected = false;
 
 const connectDB = async () => {
-  // only fields specified in scheme will be saved in database if strictQuery is true
+  // only fields specified in schema will be saved in database if strictQuery is true
   mongoose.set('strictQuery', true);
   // if connected to MongoDB, don't connect again
   if (connected) {
