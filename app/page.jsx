f60c94d49f3f92977bd06c8b587505a7555b7bcc
mv0x19 <49604532+mv0x19@users.noreply.github.com>
2024-03-31 01:13:01 +0200

    fix order of imports

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 import Hero from '@/components/Hero';
-import HomeProperties from '@/components/HomeProperties';
 import InfoBoxes from '@/components/infoBoxes';
+import HomeProperties from '@/components/HomeProperties';
 
 const HomePage = () => {
   return (
